406153d937017f3916fd943274140984bafa6ea1
tnstjd120 <tnstjd120@gmail.com>
2023-03-09 03:09:17 +0900

    feat: PostList 페이지 생성

---
+++ src/pages/PostList/index.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React from "react";
+
+const PostList = () => {
+  return <>Post List</>;
+};
+
+export default PostList;
