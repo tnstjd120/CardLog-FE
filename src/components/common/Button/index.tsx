1efdbabe8460af2170de0c15510f459fc22bcf5c
tnstjd120 <tnstjd120@gmail.com>
2023-03-09 03:04:40 +0900

    refactor: 디렉토리 구조 변경

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+
+import React from "react";
+
+const Button: React.FC = () => {
+  return (
+    <>
+      <button>Toggle</button>
+    </>
+  );
+};
+
+export default Button;
