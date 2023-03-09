a632353a7fc9956add9442109d5f03bcfb0735bc
tnstjd120 <tnstjd120@gmail.com>
2023-03-10 03:43:08 +0900

    chor: react-icons 패키지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
-
 import { css } from "@emotion/react";
-import { Route, Routes } from "react-router-dom";
 import { GlobalStyles } from "./styles/GlobalStyles";
-
 import SideBar from "./components/common/SideBar";
-import Home from "./pages/Home";
-import MyInfo from "./pages/MyInfo";
-import PostDetail from "./pages/PostDetail";
-import PrivateRoute from "./components/routes/PrivateRouter";
-import RouterInfo, { RouterItem } from "./components/routes/RouterInfo";
+import RoutesObject from "./components/routes";
+import { useState } from "react";
+import { useHasNav } from "./hooks/useHasNav";
+import { useLocation } from "react-router-dom";
 
 const mainStyles = css`
   display: flex;
@@ -24,32 +19,16 @@ const contentStyles = css`
 `;
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <>
       <GlobalStyles />
 
       <main css={mainStyles}>
-        <SideBar />
+        {useHasNav(pathname) && <SideBar />}
 
-        <div css={contentStyles}>
-          <Routes>
-            {RouterInfo.map((item: RouterItem) => {
-              return (
-                <Route
-                  key={item.path}
-                  path={item.path}
-                  element={
-                    item.isLoggedIn ? (
-                      <PrivateRoute>{item.element}</PrivateRoute>
-                    ) : (
-                      <>{item.element}</>
-                    )
-                  }
-                />
-              );
-            })}
-          </Routes>
-        </div>
+        <div css={contentStyles}>{RoutesObject()}</div>
       </main>
     </>
   );
