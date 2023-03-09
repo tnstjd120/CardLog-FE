4c017099441ab884903c184110d4a07c1260bc20
tnstjd120 <tnstjd120@gmail.com>
2023-03-10 03:45:26 +0900

    feat: customHook import 방식 변경

--- src/components/routes/PrivateRouter.tsx
+++ src/components/routes/PrivateRouter.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
-import useAuth from "../../hooks/useAuth";
+import { useAuth } from "../../hooks/useAuth";
 
 interface PrivateRouteProps {
   children: ReactNode;
