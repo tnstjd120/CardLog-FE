502267bd2e8d4245f7d264c8c67a7f2a65da8348
tnstjd120 <tnstjd120@gmail.com>
2023-03-10 03:46:51 +0900

    feat: useHavNav CustomHook 으로 제작

---
+++ src/hooks/useHasNav.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import RouterInfo from "../components/routes/RouterInfo";
+
+export const useHasNav = (path: string): boolean => {
+  // 혹시 이 부분을 보고 계신 분이 있을진 모르겠지만 좀 더 컴팩트한 방법을 생각 중 입니다...
+  const pathArr = path.split("/");
+  if (pathArr.length > 2) {
+    if (pathArr[1] === "posts") return true;
+  }
+
+  for (let [key, value] of Object.entries(RouterInfo)) {
+    if (value.path === path) return value.isWithNav;
+  }
+
+  return true;
+};
