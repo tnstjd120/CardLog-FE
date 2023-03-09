06261c09c2b4096655a2ef9661f51796b870f476
tnstjd120 <tnstjd120@gmail.com>
2023-03-10 03:48:13 +0900

    feat: Login Page 제작 시작

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import MobileContainer from "../../components/common/MobileContainer";
+import LogoSvg from "../../assets/logo.svg";
 
 const Login = () => {
-  return <>Login</>;
+  return (
+    <MobileContainer className="login">
+      <figure className="logo">{/* <img src={LogoSvg} alt="" /> */}</figure>
+    </MobileContainer>
+  );
 };
 
 export default Login;
