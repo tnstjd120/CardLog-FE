8fa33ab1079beca509e4300667ab8db609982fef
tnstjd120 <tnstjd120@gmail.com>
2023-03-10 03:45:54 +0900

    fix: RouterInfo 객체 타입으로 변환

--- src/components/routes/RouterInfo.tsx
+++ src/components/routes/RouterInfo.tsx
@@ -7,35 +7,49 @@ export interface RouterItem {
   path: string;
   element: JSX.Element;
   isLoggedIn: boolean;
+  isWithNav: boolean;
+  isNavIn: boolean;
   label: string;
 }
 
-const RouterInfo: RouterItem[] = [
-  {
+type RouterItemType = {
+  [key: string]: RouterItem;
+};
+
+const RouterInfo: RouterItemType = {
+  HOME: {
     path: "/",
     element: <Home />,
-    isLoggedIn: false,
     label: "홈",
+    isLoggedIn: false, // 로그인이 필요한 페이지인지
+    isWithNav: true, // Nav와 함께 나올 페이지인지
+    isNavIn: true, // Nav안에 보여줄 페이지인지
   },
-  {
+  LOGIN: {
     path: "/login",
     element: <Login />,
-    isLoggedIn: false,
     label: "로그인",
+    isLoggedIn: false,
+    isWithNav: false,
+    isNavIn: true,
   },
-  {
+  POST: {
     path: "/posts/:id",
     element: <PostDetail />,
-    isLoggedIn: false,
     label: "게시물 상세페이지",
+    isLoggedIn: false,
+    isWithNav: true,
+    isNavIn: true,
   },
-  {
+  MYINFO: {
     path: "/my",
     element: <MyInfo />,
-    isLoggedIn: true,
     label: "내 정보",
+    isLoggedIn: true,
+    isWithNav: false,
+    isNavIn: true,
   },
-];
+};
 
 Object.freeze(RouterInfo);
 
