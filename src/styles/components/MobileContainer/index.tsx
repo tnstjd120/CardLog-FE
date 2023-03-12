770f458d693cbf6db82e1775e0ef2deeccbd4db0
tnstjd120 <tnstjd120@gmail.com>
2023-03-13 02:48:08 +0900

    style: 기존 색상을 palette 객체 안의 색상으로 변경

--- src/styles/components/MobileContainer/index.tsx
+++ src/styles/components/MobileContainer/index.tsx
@@ -1,11 +1,12 @@
 import { css } from "@emotion/react";
+import { palette } from "styles/theme";
 
 export const mobileContainerStyles = css`
   width: 100%;
   height: 100vh;
 
   .inner {
-    background-color: #f5f5f5;
+    background-color: ${palette.gray0};
     height: 100vh;
     width: 100%;
     max-width: 500px;
@@ -15,7 +16,7 @@ export const mobileContainerStyles = css`
 
     .top_nav {
       height: 50px;
-      background-color: #333;
+      background-color: ${palette.black1};
       width: 100%;
       position: absolute;
       left: 0;
@@ -29,12 +30,47 @@ export const mobileContainerStyles = css`
       }
 
       h4 {
-        color: #fff;
+        color: ${palette.white};
         font-weight: 400;
         position: absolute;
         left: 50%;
         top: 50%;
         transform: translate(-50%, -50%);
+      }
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100%;
+      padding: 0 20px 50px;
+      overflow-y: auto;
+
+      figure {
+        padding: 50px 0 100px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+
+        img {
+          max-width: 100px;
+        }
+
+        figcaption {
+          padding-top: 40px;
+          text-align: center;
+          font-size: 1.2rem;
+          color: ${palette.black2};
+          font-weight: 400;
+
+          small {
+            font-size: 1rem;
+            font-weight: 300;
+            color: ${palette.black4};
+          }
+        }
       }
     }
   }
