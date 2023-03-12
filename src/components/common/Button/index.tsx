75c9419f54a4539b404472ebcc354f9d2354d1be
tnstjd120 <tnstjd120@gmail.com>
2023-03-13 02:43:12 +0900

    fix: Button 컴포넌트 모듈화

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,42 +1,52 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
-import { css } from "@emotion/react";
+import { css, SerializedStyles } from "@emotion/react";
 import { ThemeType } from "styles/emotion";
 import theme from "styles/theme";
-import { IconType } from "react-icons";
 
-interface ButtonProps {
-  themeType: ThemeType;
-  children: string | React.ReactNode;
+export interface ButtonProps {
+  themeType?: ThemeType;
+  children?: string | React.ReactElement;
+  icon?: React.ReactElement;
+  type?: "button" | "submit";
   marginRight?: string;
   size?: "sm" | "md" | "lg";
   isBorder?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  customCss?: SerializedStyles;
 }
 
 const Button: React.FC<ButtonProps> = ({
   themeType,
   children,
+  icon,
+  type,
   marginRight = 0,
   size = "md",
   isBorder = false,
+  customCss = "",
   onClick,
 }) => {
-  const buttonStyle = css`
-    font-size: 20px;
-    border: 0;
-    background-color: ${theme[themeType].backgroundColor};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    cursor: pointer;
-  `;
-
   return (
     <>
-      <button css={buttonStyle} onClick={onClick}>
-        {children}
+      <button
+        css={css`
+          font-size: 1rem;
+          border: 0;
+          background-color: ${themeType && theme[themeType].backgroundColor};
+          color: ${themeType && theme[themeType].color};
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+          transition: 0.3s;
+          ${customCss}
+        `}
+        type={type}
+        onClick={onClick}
+      >
+        {icon && icon}
+        {children && children}
       </button>
     </>
   );
