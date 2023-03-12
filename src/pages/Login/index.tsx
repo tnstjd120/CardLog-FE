771ba1342905f3bd9b1226866bfe8b4acd92d13b
tnstjd120 <tnstjd120@gmail.com>
2023-03-13 02:46:18 +0900

    fix: MobileContainer 에서 사용할 Caption props 추가

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import MobileContainer from "../../components/common/MobileContainer";
-import LogoSvg from "../../assets/logo.svg";
+import LoginForm from "components/login";
 
 const Login = () => {
+  const caption = (
+    <>
+      안녕하세요 <br />
+      CardLog 입니다.
+      <br />
+      <br />
+      <small>회원 서비스 이용을 위해 로그인 해주세요.</small>
+    </>
+  );
+
   return (
-    <MobileContainer className="login" title={"로그인"}>
-      <figure className="logo">{/* <img src={LogoSvg} alt="" /> */}</figure>
+    <MobileContainer title={"로그인"} caption={caption}>
+      <LoginForm />
     </MobileContainer>
   );
 };
