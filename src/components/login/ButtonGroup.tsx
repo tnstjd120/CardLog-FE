0c5e536d27fc3995aac2e077d30531ae87701403
tnstjd120 <tnstjd120@gmail.com>
2023-03-16 23:42:33 +0900

    fix: 회원가입 navigate

--- src/components/login/ButtonGroup.tsx
+++ src/components/login/ButtonGroup.tsx
@@ -2,8 +2,14 @@
 import { css } from "@emotion/react";
 import { palette } from "styles/theme";
 import Button, { ButtonProps } from "components/common/Button";
+import { useNavigate } from "react-router-dom";
+import RouterInfo from "components/routes/RouterInfo";
 
 const ButtonGroup = () => {
+  const navigate = useNavigate();
+
+  const { SIGNUP } = RouterInfo;
+
   const CustomButton = (props: ButtonProps) => {
     return (
       <Button
@@ -36,7 +42,9 @@ const ButtonGroup = () => {
     >
       <CustomButton>아이디 찾기</CustomButton>
       <CustomButton>비밀번호 찾기</CustomButton>
-      <CustomButton>회원가입</CustomButton>
+      <CustomButton onClick={() => navigate(SIGNUP.path)}>
+        회원가입
+      </CustomButton>
     </div>
   );
 };
