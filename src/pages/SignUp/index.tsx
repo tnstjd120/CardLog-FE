06baa987d5ad30691bd3349f1bb071d2f2e41d86
tnstjd120 <tnstjd120@gmail.com>
2023-03-16 23:43:33 +0900

    feat: 회원가입 페이지 생성

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,15 @@
+import SignUpForm from "components/\bsignup";
+import React from "react";
+import MobileContainer from "../../components/common/MobileContainer";
+
+const SignUp = () => {
+  const caption = <>회원가입</>;
+
+  return (
+    <MobileContainer title={"회원가입"} caption={caption}>
+      <SignUpForm />
+    </MobileContainer>
+  );
+};
+
+export default SignUp;
