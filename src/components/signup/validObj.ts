2fab5c8c1a5b53cbc6926caa2b03ca2c580d15f9
tnstjd120 <tnstjd120@gmail.com>
2023-03-24 05:20:54 +0900

    fix: 유효성 검증 부분 수정

--- src/components/signup/validObj.ts
+++ src/components/signup/validObj.ts
@@ -6,14 +6,6 @@ export interface validObjProps {
   };
 }
 
-// const regexObj: regexObjProps = {
-//   email:
-//     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-//   name: /^[가-힣]{2,5}$/, // 이름 2 ~ 5글자
-//   password: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/, // 비밀번호 (최소 8자리 숫자,문자,특수문자 최소 1개),
-//   phone: /01[016789]-[^0][0-9]{2,3}-[0-9]{4}/, // 휴대폰 번호 양식
-// };
-
 const validObj: validObjProps = {
   email: {
     regexp:
@@ -21,7 +13,7 @@ const validObj: validObjProps = {
     isTest: false,
     value: "",
   },
-  name: {
+  username: {
     regexp: /^[가-힣]{2,5}$/, // 이름 2 ~ 5글자
     isTest: false,
     value: "",
@@ -36,11 +28,11 @@ const validObj: validObjProps = {
     isTest: false,
     value: "",
   },
-  phone: {
-    regexp: /01[016789]-[^0][0-9]{2,3}-[0-9]{4}/, // 휴대폰 번호 양식
-    isTest: false,
-    value: "",
-  },
+  // phone: {
+  //   regexp: /01[016789]-[^0][0-9]{2,3}-[0-9]{4}/, // 휴대폰 번호 양식
+  //   isTest: false,
+  //   value: "",
+  // },
 };
 
 export default validObj;
