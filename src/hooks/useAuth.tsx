7daa21efa75d13967ea607f4451ff40b55758e92
tnstjd120 <tnstjd120@gmail.com>
2023-03-24 05:21:15 +0900

    fix: JWT Auth check 로직 변경

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,13 +1,36 @@
-import { useEffect, useState } from "react";
+import api from "libs/axios";
+import { useEffect, useState, useLayoutEffect } from "react";
+import { getCookie } from "utils/cookie/universal-cookie";
+import API_Path from "utils/path/API_Path";
 
-export const useAuth = (): boolean => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+export const useAuth = async () => {
+  const accessToken = getCookie("access");
+  console.log(accessToken);
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    setIsLoggedIn(() => (token ? true : false));
-  }, []);
-
-  return isLoggedIn;
+  if (accessToken) {
+    return true;
+  } else {
+    return await reissueAccess();
+  }
+};
+// FIXME Promise 객체 고쳐야됌
+const reissueAccess = async () => {
+  console.log("0 refresh");
+  const refreshToken = getCookie("refresh");
+  if (refreshToken) {
+    console.log(refreshToken);
+    await api
+      .post(API_Path.REFRESH_TOKEN, { refresh: refreshToken })
+      .then((res) => {
+        console.log("1 refresh");
+        return true;
+      })
+      .catch((error) => {
+        console.log(error);
+        return false;
+      });
+  } else {
+    console.log("2 refresh");
+    return false;
+  }
 };
