aee1765aaf8d395a5e0abe45706f9561251fb642
tnstjd120 <tnstjd120@gmail.com>
2023-03-24 21:22:58 +0900

    feat: AccessToken, RefreshToken JWT 로직 제작

--- src/auth/jwtAuth.ts
+++ src/auth/jwtAuth.ts
@@ -2,11 +2,12 @@ import axios from "axios";
 import api from "libs/axios";
 import { palette } from "styles/theme";
 import Swal from "sweetalert2";
-import { setCookie } from "utils/cookie/universal-cookie";
+import { getCookie } from "utils/cookie/universal-cookie";
 import API_Path from "utils/path/API_Path";
+import jwt_decode from "jwt-decode";
+import RouterInfo from "components/routes/RouterInfo";
 
-const ACCESS_EXPIRY_TIME = 60000 * 5; // 5분 accessToken
-const REFRESH_EXPIRY_TIME = 3600 * 1000 * 24; // 24시간 refreshToken
+let loginInterval: NodeJS.Timer | null = null;
 
 interface LoginProps {
   email: string;
@@ -28,8 +29,38 @@ export const login = async (data: LoginProps) => {
     });
 };
 
-export const silentRefresh = () => {};
+export const reissueAccess = async () => {
+  console.log("Access 재발급!");
+  const refreshToken = getCookie("refresh");
+
+  if (refreshToken) {
+    console.log(refreshToken);
+    await api
+      .post(API_Path.REFRESH_TOKEN, { refresh: refreshToken })
+      .then((res) => {
+        loginSuccess(res.data.access_token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    return (window.location.href = RouterInfo.LOGIN.path);
+  }
+};
 
 export const loginSuccess = (accessToken: string) => {
   axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  const decode: any = jwt_decode(accessToken);
+  const expireTime = decode.exp;
+
+  const delay = (expireTime - Date.now() / 1000) * 1000;
+
+  loginInterval = setInterval(reissueAccess, delay - 30000);
+};
+
+export const logout = async () => {
+  if (loginInterval !== null) clearInterval(loginInterval);
+
+  await api.post(API_Path.LOGOUT);
 };
