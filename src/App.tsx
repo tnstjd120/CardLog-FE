7c637da924aa8aef4a51b5cab598fead6a67341b
tnstjd120 <tnstjd120@gmail.com>
2023-03-25 23:01:12 +0900

    style: mainStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,24 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 import { useHasNav } from "./hooks/useHasNav";
 import { useLocation } from "react-router-dom";
 
+import { useSelector, useDispatch } from "react-redux";
+import user, { setUser } from "store/user";
+
 import SideBar from "./components/common/SideBar";
 import RoutesObject from "./components/routes";
+import { api } from "libs/axios";
+import API_Path from "utils/path/API_Path";
+import { useUserInfo } from "hooks/useUserInfo";
+import { RootState } from "store";
 
 const mainStyles = css`
   display: flex;
-  height: 100vh;
+  width: 100%;
+  max-width: 1200px;
+  height: calc(100vh);
+  margin: 0 auto;
+  border-radius: 14px;
+  box-shadow: 0 3px 12px rgba(0, 0, 0, 0.1);
 `;
 
 const contentStyles = css`
@@ -19,7 +31,7 @@ const contentStyles = css`
 
 function App() {
   const { pathname } = useLocation();
-
+  useUserInfo();
   return (
     <>
       <GlobalStyles />
