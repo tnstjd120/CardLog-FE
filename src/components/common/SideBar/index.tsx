0cd0deb1471547f35e30c5dd7bc01d7640929232
tnstjd120 <tnstjd120@gmail.com>
2023-03-25 23:02:43 +0900

    fix: SideBar 레이아웃 변경

--- src/components/common/SideBar/index.tsx
+++ src/components/common/SideBar/index.tsx
@@ -2,12 +2,32 @@
 
 import React from "react";
 import { sideBarStyle } from "../../../styles/components/SideBar";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import RouterInfo, { RouterItem } from "../../routes/RouterInfo";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
+import { UserState } from "store/user";
 
 const SideBar: React.FC = (): JSX.Element => {
+  const userInfo = useSelector<RootState>((state) => state.user) as UserState;
+  console.log(userInfo);
   return (
     <article css={sideBarStyle}>
+      <h2>
+        <Link to={RouterInfo.HOME.path}>{userInfo.blog_name}</Link>
+      </h2>
+
+      <figure>
+        <div className="img_wrap">
+          <img
+            src="https://velog.velcdn.com/images/tnstjd120/profile/e1994e20-250c-4517-9c00-281ee42cd780/image.jpeg"
+            alt=""
+          />
+        </div>
+
+        <figcaption>{userInfo.username}</figcaption>
+      </figure>
+
       <nav>
         {Object.entries(RouterInfo).map(
           ([key, item]: [string, RouterItem]) =>
