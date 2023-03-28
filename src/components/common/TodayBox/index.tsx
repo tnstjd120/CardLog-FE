9e198e22501abcdc68f7126b197f03da6e11cf72
tnstjd120 <tnstjd120@gmail.com>
2023-03-28 14:03:17 +0900

    feat: TodayBox 컴포넌트 생성

---
+++ src/components/common/TodayBox/index.tsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { palette } from "styles/theme";
+
+const todayStyles = css`
+  position: absolute;
+  left: 30px;
+  bottom: 30px;
+  font-size: 16px;
+  font-weight: 300;
+
+  & > span:first-of-type {
+    margin-right: 30px;
+  }
+`;
+
+const TodayBox = () => {
+  return (
+    <div css={todayStyles}>
+      <span>Today - 274</span>
+      <span>All - 5327</span>
+    </div>
+  );
+};
+
+export default TodayBox;
