c111d6966188b3b5fd3a046bcd5439a73e9ba456
tnstjd120 <tnstjd120@gmail.com>
2023-04-01 01:57:06 +0900

    fix: 게시글 올린 시간 초 단위 까지 확대하는 기능 제작

--- src/utils/date/days.tsx
+++ src/utils/date/days.tsx
@@ -2,15 +2,34 @@ export const days = (date: string) => {
   const today = new Date();
   const targetDate = new Date(date);
   const timeDiff = Math.abs(today.getTime() - targetDate.getTime());
-  const diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
 
-  if (diffDays >= 7) {
-    const year = targetDate.getFullYear();
-    const month = String(targetDate.getMonth() + 1).padStart(2, "0");
-    const day = String(targetDate.getDate()).padStart(2, "0");
-
-    return `${year}년 ${month}월 ${day}일`;
+  // 초 전
+  const diffSeconds = Math.floor(timeDiff / 1000);
+  if (diffSeconds < 60) {
+    return `${diffSeconds}초 전`;
   }
 
-  return `${diffDays}일 전`;
+  // 분 전
+  const diffMinutes = Math.floor(timeDiff / (1000 * 60));
+  if (diffMinutes < 60) {
+    return `${diffMinutes}분 전`;
+  }
+
+  // 시간 전
+  const diffHours = Math.floor(timeDiff / (1000 * 3600));
+  if (diffHours < 24) {
+    return `${diffHours}시간 전`;
+  }
+
+  // 일 전
+  const diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+  if (diffDays < 7) {
+    return `${diffDays}일 전`;
+  }
+
+  // 년-월-일
+  const year = targetDate.getFullYear();
+  const month = String(targetDate.getMonth() + 1).padStart(2, "0");
+  const day = String(targetDate.getDate()).padStart(2, "0");
+  return `${year}년 ${month}월 ${day}일`;
 };
