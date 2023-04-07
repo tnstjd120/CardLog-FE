e7dfa31bbe2ab500e2e477d61bc6ca09c5d1bf9c
tnstjd120 <tnstjd120@gmail.com>
2023-04-07 16:13:51 +0900

    refactor: 사용하지 않는 요소 제거

--- src/libs/axios/index.ts
+++ src/libs/axios/index.ts
@@ -3,9 +3,6 @@ import { getCookie } from "utils/cookie/universal-cookie";
 
 const API_BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const params = new URLSearchParams(window.location.search);
-const blogId = params.get("blog_id");
-
 export const api = axios.create({
   baseURL: API_BASE_URL || "http://localhost:8000/",
   withCredentials: true,
