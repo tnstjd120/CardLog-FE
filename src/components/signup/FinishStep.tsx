9a5d33bd7a4cb2b105b280bd43501bd116520f3b
tnstjd120 <tnstjd120@gmail.com>
2023-05-14 01:07:19 +0900

    chor: 텍스트 내용 수정

--- src/components/signup/FinishStep.tsx
+++ src/components/signup/FinishStep.tsx
@@ -12,8 +12,8 @@ const FinishStep = ({ setSignUpStep, signUpInfo }: secondStepProps) => {
   return (
     <>
       <p css={FinishStepStyles}>
-        입력하신 이메일로 인증메일을 전송해드렸습니다 <br /> <br />
-        이메일 인증 완료 후 로그인이 가능합니다
+        입력하신 이메일로 인증메일을 전송해드렸습니다. <br /> <br />
+        이메일 인증 완료 후 로그인이 가능합니다.
       </p>
     </>
   );
